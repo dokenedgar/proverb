4fec28942de69221b599c15bfe19a479007f6e11
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-29 17:25:05 +0100

    Add solution to exercise

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+    let mut index = 0;
+    let mut proverb: String = String::new();
+    for item in list {
+        if index == list.len() - 1 {
+            if list.len() == 1 {
+                proverb.push_str(format!("And all for the want of a {}.", list[0]).as_str());
+            } else {
+                proverb.push_str(format!("\nAnd all for the want of a {}.", list[0]).as_str());
+            }
+        } else {
+            proverb = add_str(proverb, &item, list[index + 1], index);
+            index += 1;
+        }
+    }
+
+    proverb
+}
+
+fn add_str(mut initial_string: String, item: &str, next: &str, index: usize) -> String {
+    if index > 0 {
+        initial_string
+            .push_str(format!("\nFor want of a {} the {} was lost.", item, next).as_str());
+    } else {
+        initial_string.push_str(format!("For want of a {} the {} was lost.", item, next).as_str());
+    }
+    initial_string
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+fn main() {
+    let list = [
+        "nail", "shoe", "horse", "rider", "message", "battle", "kingdom",
+    ];
+    let res = proverb::build_proverb(&list);
+    print!("{res}");
+}
